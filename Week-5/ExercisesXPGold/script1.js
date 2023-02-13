1dcc8f945bd8dcbd55de06dda46d5102f454bbcb
Klair8 <clsacuto@gmail.com>
2023-02-13 09:33:07 +0200

    exerciseXP

--- Week-5/ExercisesXPGold/script1.js
+++ Week-5/ExercisesXPGold/script1.js
@@ -1,10 +1,11 @@
 
 const string2 = localStorage.getItem('data')
 
-// const data2 =JSON.parse(string2);
-// console.log(data2);
+const data2 =JSON.parse(string2);
+console.log(data2);
 
 const dataV = document.createElement("p");
-const valueD = document.createTextNode(localStorage.getItem('data'));
+const valueD = document.createTextNode(data2.name + " " + data2.lastN);
+
 dataV.appendChild(valueD);
 document.body.appendChild(dataV);
