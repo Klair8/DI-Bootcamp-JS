005c9afeb6d1b0b2a8eb22d88de65af68c65cfdd
Klair8 <clsacuto@gmail.com>
2023-02-16 00:56:47 +0200

    exo

--- Week-5/Day-3/ExercisesXP-Gold/script.js
+++ Week-5/Day-3/ExercisesXP-Gold/script.js
@@ -17,10 +17,10 @@ const urls = [
 Promise.all(urls.map(url => {
     return fetch(url).then(resp => resp.json())
 })).then(results => {
-    console.log(results[0])
-    console.log(results[1])
-    console.log(results[2])
-      console.log(results[3])
+    console.log(results[0].result.properties)
+    console.log(results[1].result.properties)
+    console.log(results[2].result.properties)
+      console.log(results[3].result.properties)
 }).catch(() => console.log('error'))
 
 
---
+++ Week-5/Day-4/Daily-Challenge/script.js
@@ -0,0 +1,52 @@
+// 1st Challenge
+// Instructions
+// Use Promise.all
+// This function should accept an array of promises and 
+// return an array of resolved values.
+// If any of the promises are rejected, 
+// the function should catch them.
+
+// Explain in a comment how Promise.all work and why you receive this output.
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, 'foo');
+});
+
+Promise.all([promise1,promise2,promise3])
+.then(values=>{
+    console.log(values);
+})
+.catch((){
+    throw Error ('error')
+})
+
+
+//the promise.all take an array of Promises and returns a new promise.
+//the order of the resulting  is the same as in its source promises.
+
+// 2nd Challenge
+// Instructions
+// You will find the hour of sunrise of two cities,
+//  using the API https://sunrise-sunset.org/api.
+
+// In the HTML file, create a form with 4 inputs:
+// the latitude and longitude of the first city
+// the latitude and longitude of the second city
+
+// Retrieve the input’s value and display the hour of the sunrise
+//  for both city ONLY when both promises are resolved
+// Hint : Use Promise.all()
+
+
+// Try with Paris and New York
+
+// Paris
+// Latitude: 48.864716
+// Longitude: 2.349014
+
+// New York
+// Latitude: 40.730610
+// Longitude: -73.935242
+
