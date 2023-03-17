4d48660ed29f78a4604d09e6552d6ca838412909
Klair8 <clsacuto@gmail.com>
2023-03-17 10:15:04 +0200

    posterdone

--- Week-9/Day-5/Daily-Challenge/movie-app/src/components/MovieCard.js
+++ Week-9/Day-5/Daily-Challenge/movie-app/src/components/MovieCard.js
@@ -3,11 +3,14 @@ import React from 'react';
 
 const MovieCard = ({ Title,Year,Poster }) => {
   return(
-    <div>
+    <div style={{border: '2px.solid black'}}>
         <img src={Poster} alt={Title} />
         <div>
             <h2>{Title}</h2>
             <p> {Year}</p>
+            {/* <Link to={`/movies/${movie.imdbID}`} className="btn"> 
+              Details
+            </Link>  */}
         </div>
     </div>
 )
--- Week-9/Day-5/Daily-Challenge/movie-app/src/components/MoviesContainer.js
+++ Week-9/Day-5/Daily-Challenge/movie-app/src/components/MoviesContainer.js
@@ -3,16 +3,6 @@ import MovieCard from './MovieCard';
 
 const MoviesContainer =({movies}) =>{
 
-  // <img src={movie.Poster} alt={movie.Title} />
-  //     <div className="movie-info">
-  //       <h3>{movie.Title}</h3>
-  //       <span>{movie.Year}</span>
-  //     </div>
-  //     <Link to={`/movies/${movie.imdbID}`} className="btn"> 
-  //       Details
-  //     </Link> 
-
-
   return (
     <div>
       {movies && movies.map((elem, i)=>{
@@ -21,7 +11,8 @@ const MoviesContainer =({movies}) =>{
             key={i} 
             id={movies[i].id} 
             Title={movies[i].Title} 
-            Year={movies[i].Year} />
+            Year={movies[i].Year} 
+            Poster={movies[i].Poster}/>
           )
       })  
       }
