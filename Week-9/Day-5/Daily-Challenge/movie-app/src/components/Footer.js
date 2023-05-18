703f346e30f459dffedc24f4ed32d76b53566415
Klair8 <clsacuto@gmail.com>
2023-05-18 12:24:14 +0300

    footer done

--- Week-9/Day-5/Daily-Challenge/movie-app/src/components/Footer.js
+++ Week-9/Day-5/Daily-Challenge/movie-app/src/components/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const FooterOne = () => {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+      <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Copyright:
+        Claire Using React JS & Redux JS integrated with external movies data API
+        <a className="text-dark" href="https://www.omdbapi.com/">OMDB</a>
+      </div>
+    </footer>
+  );
+};
 
-const Footer =()=>{
-  return(
-    <h4>By @Claire  Using React JS & Redux JS integrated with external movies data APIOMDB</h4>
-  )
-}
-
-
-export default Footer
+export default FooterOne;
